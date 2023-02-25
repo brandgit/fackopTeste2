445e5da470392df6a9bde40f0fbb90d185340620
Brand TIKOUE TIKOUE <93736627+brandgit@users.noreply.github.com>
2023-02-25 10:46:38 +0100

    Comit logo

--- src/components/global/Logo.vue
+++ src/components/global/Logo.vue
@@ -1,3 +1,3 @@
 <template>
-    <router-link to="/"><img class=" w-[90px] sm:w-[120px]" src="logos/LogoFackop6.0.png" alt="logo-fackop"></router-link>
+    <router-link to="/"><img class=" w-[90px] sm:w-[120px]" src="/logos/LogoFackop6.0.png" alt="logo-fackop"></router-link>
 </template>
