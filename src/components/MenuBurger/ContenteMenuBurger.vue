698852ad1f62aa0c7513cac0531c576028980f77
Brand TIKOUE TIKOUE <93736627+brandgit@users.noreply.github.com>
2023-03-04 04:14:31 +0100

    Commit du 04/03/2023

--- src/components/MenuBurger/ContenteMenuBurger.vue
+++ src/components/MenuBurger/ContenteMenuBurger.vue
@@ -1,54 +1,44 @@
 <template>
     <div @click="modalHandler()" class="flex fixed w-full h-screen z-20  top-[80px] left-0 ease-out duration-500 bg-[rd] " :class="[stateManu ? 'left-[-100%]' : 'left-0']" >
-        <div class="w-[300px] h-full bg-black bg-opacity-50 backdrop-blur-[15px]">
+        <div class="flex flex-col w-full h-full bg-black bg-opacity-50 backdrop-blur-[15px] ">
+          
+            <div class="flex flex-col gap-10 h-[75%] bg-black">
+                <div class="ml-[5vw] h-[130px] w-[95%]">
+                    <div class="flex font-bold items-center cursor-pointer mt-5 text-[15px]">
+                        <h1>MENU</h1>
+                    </div>
+                    <div class="flex flex-col justify-evenly h-full text-[12px] mt-3 ml-5 text-[#d2caca]">
+                        <router-link  to="/"><h1>Home</h1></router-link>
+                        <router-link to="/services"><h1>Services</h1></router-link>
+                        <router-link to="/about"><h1>About</h1></router-link>
+                        <router-link to="/contact"><h1>Contacts</h1></router-link>
+                    </div>
+                </div>
 
+                <div class="ml-[5vw] h-[185px] w-[95%]">
+                    <div class="flex font-bold items-center cursor-pointer mt-5 text-[15px]">
+                        <h1>ALL FACKOP RESSOURCES</h1>
+                    </div>
+                    <div class="flex flex-col justify-evenly h-full text-[12px] mt-3 ml-5 text-[#d2caca]">
+                        <router-link  to="#"><h1>Allô-chop</h1></router-link>
+                        <router-link to="#"><h1>Clando</h1></router-link>
+                        <router-link to="#"><h1>Falla</h1></router-link>
+                        <router-link to="#"><h1>Kidangui</h1></router-link>
+                        <router-link to="#"><h1>Ogoula</h1></router-link>
+                        <router-link to="#"><h1>Pôpô</h1></router-link>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-end w-[100%] h-[25%]">
+                <div class='flex w-full h-full text-[14px] border-t-[#d2caca] border-solid border-t-[2px] text-[#d2caca]'>
+                    <div class='flex text-xs mt-[4%] flex-col items-center w-[100%] '>
+                        <p>Copyright - Fackop © 2023 Inc.</p>
+                        <p class="sm:ml-5">Tous droits réservés.</p>
+                        <p class="sm:ml-5">Mentions légales</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- <div class="flex flex-col items-center w-full bg-white">
-            <div class="w-[90%] bg-yellow-400"> -->
-                <!-- :class="[stateManu ? 'notActive' : 'active']" -->
-                <!-- <TitleH2Vue title="Menu"/> -->
-
-            <!-- </div>
-        </div> -->
-        <!-- <div class="active   transition  font-mono">
-  
-</div> -->
-        <!-- <div class="w-[300px] text-black bg-[red] sm">
-            <div >
-                           <div class="ml-[5vw] h-[200px] w-[95%]">
-                                <div class="flex flex-col justify-evenly h-full sm:text-[15px] ">
-                                    <router-link  to="/"><h1>Home</h1></router-link>
-                                    <router-link to="/services"><h1>Services</h1></router-link>
-                                    <router-link to="/about"><h1>About</h1></router-link>
-                                    <router-link to="/contact"><h1>Contacts</h1></router-link>
-                                </div>
-                           </div>
-                           <div class="ml-[5vw] h-[200px] w-[95%] ">
-                                <div class="flex font-bold items-center cursor-pointer text-[11px] sm:text-[15px] ">
-                                    <h1 >All Fackop ressources</h1>
-                                </div>
-                                <div class="flex text-[#686565]  ml-4 flex-col justify-evenly h-[190px] ">
-                                    <router-link  to="#"><h1>Allô-chop</h1></router-link>
-                                    <router-link to="#"><h1>Clando</h1></router-link>
-                                    <router-link to="#"><h1>Falla</h1></router-link>
-                                    <router-link to="#"><h1>Kidangui</h1></router-link>
-                                    <router-link to="#"><h1>Ogoula</h1></router-link>
-                                    <router-link to="#"><h1>Pôpô</h1></router-link>
-                                </div>
-                           </div>
-                           <div class="flex items-end w-[100%] h-[200px] ">
-                                <div class='flex w-full text-[14px] text-black'>
-                                    <div class='flex text-xs flex-col  pt-5 pb-3 items-center w-[100%] border-t-black border-solid border-t-[2px]'>
-                                        <p>Copyright - Fackop © 2023 Inc.</p>
-                                        <p class="sm:ml-5">Tous droits réservés.</p>
-                                        <p class="sm:ml-5">Mentions légales</p>
-                                    </div>
-                                </div>
-                           </div>
-                        </div>
-                        
-                        <div v-else class="none"></div>
-        </div> -->
     </div>
 </template>
 
@@ -67,6 +57,14 @@ import { computed } from 'vue';
 </script>
 
 <style scoped>
+.wrapperHome {
+  overflow: hidden;
+  overflow-y: scroll;
+}
+.wrapperHome::-webkit-scrollbar {
+  background-color: transparent;
+  width: 0px;
+}
  .active {
     width: 100%;
     height: 100vh;
